8c7f1f8eb267c5d03ac6035d99443f48a6b3a59d
Mayara Maria <mayara10039@hotmail.com>
2023-01-21 22:05:29 -0300

    "Desafio Concluído"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone ip1 = new Iphone("123456789", "Xs", "5588mk", 15);
+ip1.Ligar();
+ip1.ReceberLigacao();
+ip1.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nok1 = new Nokia("987654321", "MJ", "535f52s", 10);
+nok1.Ligar();
+nok1.InstalarAplicativo("Whatssapp");
